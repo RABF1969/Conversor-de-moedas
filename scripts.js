7fc46ea7a7cd42a58adde46626ac19e66c718853
rabf1969 <rabf44@gmail.com>
2025-02-09 20:10:44 -0300

    segunda-alteracao

--- scripts.js
+++ scripts.js
@@ -2,22 +2,34 @@ const convertButton = document.querySelector(".convert-button");
 
 function convertValues() {
   const inputCurrencyValue = document.querySelector(".input-currency").value;
-  const currenceValueToConvert = document.querySelector(".currency-value-to-convert"); // valor em Real
-  const currenceValueToConverted = document.querySelector(".currency-value"); // outras moedas
+  const currenceValueToConvert = document.querySelector(".currency-value-to-convert" ); // valor em Real
+  const currenceValueConverted = document.querySelector(".currency-value"); // outras moedas
+  const currencySelect = document.querySelector(".currency-select");
 
+  
   const dolarToday = 5.2;
+  const euroToday = 6.2;
 
-  const convertedvalue = inputCurrencyValue / dolarToday;
+  if (currencySelect.value == "dolar") {
+    // Se o select estiver selecionado o valor do dolar, entre aqui
+    currenceValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputCurrencyValue / dolarToday);
+  }
+
+  if (currencySelect.value == "euro") {
+    // Se o select estiver selecionado o valor do euro, entre aqui
+    currenceValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputCurrencyValue / euroToday);
+  }
 
   currenceValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
-    currency: "BRL",  
+    currency: "BRL",
   }).format(inputCurrencyValue);
-
-  currenceValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(convertedvalue);
 
   console.log(convertedvalue);
 }
