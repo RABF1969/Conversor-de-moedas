66ec63dd4bcf306e8539285784c6bbc837faca0b
RABF1969 <rabf44@gmail.com>
2025-03-17 16:02:16 -0300

    incrementado as moedas dinamicas

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
 const convertButton = document.querySelector(".convert-button");
 const currencySelect = document.querySelector(".currency-select");
 
-function convertValues() {
+async function convertValues() {
   const inputCurrencyValue = document.querySelector(".input-currency").value;
   const currenceValueToConvert = document.querySelector(
     ".currency-value-to-convert"
   ); // valor em Real
   const currenceValueConverted = document.querySelector(".currency-value"); // outras moedas
 
-  const dolarToday = 5.2;
-  const euroToday = 6.2;
-  const libraToday = 7.2;
+  const data = await fetch(
+    "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,GBP-BRL"
+  ).then((response) => response.json());
+
+  const dolarToday = data.USDBRL.high;
+  const euroToday = data.EURBRL.high;
+  const poundToday = data.GBPBRL.high;
 
   if (currencySelect.value == "dolar") {
     // Se o select estiver selecionado o valor do dolar, entre aqui
@@ -36,30 +40,29 @@ function convertValues() {
     }).format(inputCurrencyValue / libraToday);
   }
 
-
   currenceValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
   }).format(inputCurrencyValue);
 }
 
-function changeCurrency(){
+function changeCurrency() {
   const currencyName = document.getElementById("currency-name");
   const currencyImg = document.querySelector(".currency-img");
 
   if (currencySelect.value == "dolar") {
-    currencyName.innerHTML = "Dólar americano"  
-    currencyImg.src = "./assets/dolar.png"  
+    currencyName.innerHTML = "Dólar americano";
+    currencyImg.src = "./assets/dolar.png";
   }
 
   if (currencySelect.value == "euro") {
-    currencyName.innerHTML = "Euro"  
-    currencyImg.src = "./assets/euro.png"  
+    currencyName.innerHTML = "Euro";
+    currencyImg.src = "./assets/euro.png";
   }
 
   if (currencySelect.value == "libra") {
-    currencyName.innerHTML = "Libra"  
-    currencyImg.src = "./assets/libra.png"  
+    currencyName.innerHTML = "Libra";
+    currencyImg.src = "./assets/libra.png";
   }
 
   convertValues();
